ac804d8d16dcb605e1a92f68cbafc342c755ef4e
Pau Azpeitia <pau.apa.azpeitia@gmail.com>
2024-10-27 11:37:31 +0100

    Primer cambio prueba

--- solutions/01-AllTheColors/Program.cs
+++ solutions/01-AllTheColors/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CommandLine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                Console.WriteLine($"¡Hola, {o.Name}! Bienvenido al programa.");
+            })
+            .WithNotParsed(errors =>
+            {
+                Console.WriteLine("Error al analizar los argumentos. Asegúrese de proporcionar su nombre.");
+            });
+    }
+}
+
