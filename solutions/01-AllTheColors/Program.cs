191cd321b3f4d77c262c0f16ce0a6609ef7c25d2
Pau Azpeitia <pau.apa.azpeitia@gmail.com>
2024-11-03 19:41:16 +0100

    Last changes remeaning

--- solutions/01-AllTheColors/Program.cs
+++ solutions/01-AllTheColors/Program.cs
@@ -2,16 +2,18 @@ using System;
 using CommandLine;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing; 
+using System.Collections.Generic;
 
 public class Options
 {
     [Option('t', "trivialMode", Required = false, Default = true, HelpText = "Trivial mode?")]
     public bool TrivialMode { get; set; }
 
-    [Option('r', "randomMode", Required = false, Default = false, HelpText = "Random mode?")]
+    [Option('r', "randomMode", Required = false, Default = true, HelpText = "Random mode?")]
     public bool RandomMode { get; set; }
 
-    [Option('o', "ornamentMode", Required = false, Default = false,  HelpText = "Pattern?")]
+    [Option('o', "ornamentMode", Required = false, Default = true,  HelpText = "Pattern?")]
     public bool OrnamentMode { get; set; }
 }
 
@@ -53,7 +55,7 @@ class Program
 
                 Console.WriteLine("Image trivial.png created successfully.");
             }
-            if(o.RandomMode = true) //Random mode
+            if(o.RandomMode) //Random mode
             {
                 
                 int width = 4096;
@@ -101,16 +103,55 @@ class Program
                 Console.WriteLine("Image random.png created successfully.");
 
             }
-            if(o.ornamentMode)
+            if (o.OrnamentMode) // Ornament mode
             {
+                int width = 4096;
+                int height = 4096;
+
+                List<Rgba32> colors = new List<Rgba32>();
+                for (int r = 0; r < 256; r++)
+                {
+                    for (int g = 0; g < 256; g++)
+                    {
+                        for (int b = 0; b < 256; b++)
+                        {
+                            colors.Add(new Rgba32((byte)r, (byte)g, (byte)b));
+                        }
+                    }
+                }
+
+               // Ordenar los colores por luminosidad (más oscuros a más claros)
+            colors.Sort((c1, c2) =>
+            {
+                // Calcular la luminosidad
+                double luminance1 = 0.2126 * c1.R + 0.7152 * c1.G + 0.0722 * c1.B;
+                double luminance2 = 0.2126 * c2.R + 0.7152 * c2.G + 0.0722 * c2.B;
+
+                // Ordenar de menor a mayor luminosidad
+                return luminance1.CompareTo(luminance2);
+            });
+
+
+
                 using (var image1 = new Image<Rgba32>(width, height))
                 {
-                    //TODO
+                    int colorIndex = 0;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            image1[x, y] = colors[colorIndex];
+                            colorIndex++;
+                        }
+                    }
                     image1.Save("ornament.png");
 
                 }
-                Console.WriteLine("Image ornament.png created successfully.")
+                Console.WriteLine("Image ornament.png created successfully.");
             }
+
+
+        
         });
     }
 }
