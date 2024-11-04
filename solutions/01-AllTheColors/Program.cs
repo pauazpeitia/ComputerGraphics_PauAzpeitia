59cf23339f61103194e4b2f497d8677e0813a6b6
Pau Azpeitia <pau.apa.azpeitia@gmail.com>
2024-11-04 17:16:45 +0100

    Corrections on the argumentspassing

--- solutions/01-AllTheColors/Program.cs
+++ solutions/01-AllTheColors/Program.cs
@@ -7,29 +7,17 @@ using System.Collections.Generic;
 
 public class Options
 {
-    [Option('t', "trivialMode", Required = false, Default = 1, HelpText = "Trivial mode?")]
-    public int TrivialMode { get; set; }
+    [Option('m', "Mode", Required = true, HelpText = "Trivial mode: 1, Random mode: 2, Ornament mode: 3")]
+    public int Mode { get; set; }
 
-    [Option('r', "randomMode", Required = false, Default = 1, HelpText = "Random mode?")]
-    public int RandomMode { get; set; }
-
-    [Option('o', "ornamentMode", Required = false, Default = 1,  HelpText = "Pattern?")]
-    public int OrnamentMode { get; set; }
+    [Option('o', "Output", Required = true, HelpText = "Out put file name with .png extension")]
+    public string OutputFileName { get; set; }
 
     [Option('w', "width", Required = false, Default = 4096, HelpText = "Image width in pixels.")]
     public int Width { get; set; }
 
     [Option('h', "height", Required = false, Default = 4096, HelpText = "Image height in pixels.")]
     public int Height { get; set; }
-
-    [Option("trivialFileName", Required = false, Default = "trivial.png", HelpText = "File name for trivial mode")]
-    public string TrivialFileName { get; set; }
-
-    [Option("randomFileName", Required = false, Default = "random.png", HelpText = "File name for random mode")]
-    public string RandomFileName { get; set; }
-
-    [Option("ornamentFileName", Required = false, Default = "ornament.png", HelpText = "File name for ornament mode")]
-    public string OrnamentFileName { get; set; }
 }
 
 class Program
@@ -39,24 +27,44 @@ class Program
         Parser.Default.ParseArguments<Options>(args)
         .WithParsed<Options>(o =>
         {
+            if (o.Mode < 1 || o.Mode > 3)
+            {
+                throw new ArgumentException("Mode should be 1 (Trivial mode), 2(Random mode) or 3(ornamentmode)");
+            }
+            if (!o.OutputFileName.EndsWith(".png"))
+            {
+                throw new ArgumentException("Name of the output file should have the extension .png.");
+            }
             //Indicate wich Mode is activated
-            Console.WriteLine($"TrivialMode: {o.TrivialMode}");
-            Console.WriteLine($"RandomMode: {o.RandomMode}");
-            Console.WriteLine($"OrnamentMode: {o.OrnamentMode}");
+            Console.WriteLine($"Mode {o.Mode} activated");
 
             int width = o.Width;
             int height = o.Height;
             int maxcombinationcolors = 16777216;
-            int colorIndex = 0;
 
             if (width * height < maxcombinationcolors)
             {
                 throw new ArgumentException("The image must contain at least 2^24 pixels.");
             }
-
-            if (o.TrivialMode == 1) // Trivial mode
+            switch (o.Mode)
             {
-                using (var image1 = new Image<Rgba32>(width, height))
+                case 1: // Mode Trivial
+                    CreateTrivialImage(width, height, o.OutputFileName);
+                    break;
+                case 2: // Mode Random
+                    CreateRandomImage(width, height, o.OutputFileName);
+                    break;
+                case 3: // Mode Ornament
+                    CreateOrnamentImage(width, height, o.OutputFileName);
+                    break;
+            }
+
+        });
+    }     
+
+    static void CreateTrivialImage(int width,int height,string fileName)
+    {
+        using (var image = new Image<Rgba32>(width, height))
                 {
                     int pixelIndex = 0;
                     for (int r = 0; r < 256; r++)
@@ -67,20 +75,22 @@ class Program
                             {
                                 int x = pixelIndex % width;
                                 int y = pixelIndex / width;
-                                image1[x, y] = new Rgba32((byte)r, (byte)g, (byte)b);
+                                image[x, y] = new Rgba32((byte)r, (byte)g, (byte)b);
                                 pixelIndex++;
                             }
                         }
                     }
-                    image1.Save(o.TrivialFileName);
+                    image.Save(fileName);
                 }
 
-                Console.WriteLine($"Image {o.TrivialFileName} created successfully.");
-            }
-
-            if (o.RandomMode == 1) // Random mode
-            {
-                List<Rgba32> colors = new List<Rgba32>();
+                Console.WriteLine($"Image {fileName} created successfully.");
+    }
+    
+    static void CreateRandomImage(int width, int height, string fileName)
+    {
+        int maxcombinationcolors = 16777216;
+        int colorIndex = 0;
+        List<Rgba32> colors = new List<Rgba32>();
                 for (int r = 0; r < 256; r++)
                 {
                     for (int g = 0; g < 256; g++)
@@ -103,13 +113,13 @@ class Program
                     colors[n] = value;
                 }
 
-                using (var image2 = new Image<Rgba32>(width, height))
+                using (var image = new Image<Rgba32>(width, height))
                 {
                     for (int y = 0; y < height; y++)
                     {
                         for (int x = 0; x < width; x++)
                         {
-                            image2[x, y] = colors[colorIndex];
+                            image[x, y] = colors[colorIndex];
                             if (colorIndex < maxcombinationcolors - 1)
                             {
                                 colorIndex++;
@@ -120,14 +130,15 @@ class Program
                             }
                         }
                     }
-                    image2.Save(o.RandomFileName);
+                    image.Save(fileName);
                 }
-                Console.WriteLine($"Image {o.RandomFileName} created successfully.");
-            }
+                Console.WriteLine($"Image {fileName} created successfully.");
+    }
 
-            if (o.OrnamentMode == 1) // Ornament mode
-            {
-                List<Rgba32> colors1 = new List<Rgba32>();
+    static void CreateOrnamentImage(int width, int height, string fileName)
+    {
+        int colorIndex = 0;
+        List<Rgba32> colors1 = new List<Rgba32>();
                 for (int r = 0; r < 256; r++)
                 {
                     for (int g = 0; g < 256; g++)
@@ -139,22 +150,20 @@ class Program
                     }
                 }
 
-                using (var image3 = new Image<Rgba32>(width, height))
+                using (var image = new Image<Rgba32>(width, height))
                 {
                     int x = width / 2;
                     int y = height / 2;
                     colorIndex = 0; 
 
-                    image3[x, y] = colors1[colors1.Count - 1]; 
-                    FillAroundPixel(image3, x, y, colors1);
+                    image[x, y] = colors1[colors1.Count - 1]; 
+                    FillAroundPixel(image, x, y, colors1);
 
-                    image3.Save(o.OrnamentFileName);
+                    image.Save(fileName);
                 }
-                Console.WriteLine($"Image {o.OrnamentFileName} created successfully.");
-            }
-        });
+                Console.WriteLine($"Image {fileName} created successfully.");
     }
-
+    
     static void FillAroundPixel(Image<Rgba32> image, int startX, int startY, List<Rgba32> colors)
     {
         int width = image.Width;
